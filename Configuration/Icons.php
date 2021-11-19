b732a6a1285f00d5c7a498bef996f182d99ae355
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-11-19 11:55:40 +0100

    [TASK] Register icons in Configuration/Icons.php for TYPO3 v11

---
+++ Configuration/Icons.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "form_country_select" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+return [
+    'form-country-select' => [
+        'provider' => TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:' . Brotkrueml\FormCountrySelect\Extension::KEY . '/Resources/Public/Icons/form-country-select.svg',
+    ],
+];
--- ext_tables.php
+++ ext_tables.php
@@ -1,15 +1,17 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-(function () {
-    $iconRegistry = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        TYPO3\CMS\Core\Imaging\IconRegistry::class
-    );
-    $iconRegistry->registerIcon(
-        'form-country-select',
-        TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => 'EXT:' . Brotkrueml\FormCountrySelect\Extension::KEY . '/Resources/Public/Icons/form-country-select.svg']
-    );
+(static function () {
+    if ((new TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() < 11) {
+        $iconRegistry = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            TYPO3\CMS\Core\Imaging\IconRegistry::class
+        );
+        $iconRegistry->registerIcon(
+            'form-country-select',
+            TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            ['source' => 'EXT:' . Brotkrueml\FormCountrySelect\Extension::KEY . '/Resources/Public/Icons/form-country-select.svg']
+        );
+    }
 
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:form/Resources/Private/Language/Database.xlf'][]
         = 'EXT:' . Brotkrueml\FormCountrySelect\Extension::KEY . '/Resources/Private/Language/Backend.xlf';
