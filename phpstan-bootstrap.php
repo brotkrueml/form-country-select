06fe81133fcebfa33c19eb8e9f9c57580c0eaa87
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-03-12 19:00:29 +0100

    [TASK] Add saschaegerer/phpstan-typo3

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -1,8 +1,5 @@
 <?php
 
-// To avoid "Constant TYPO3_MODE not found" in CountrySelect class
-define('TYPO3_MODE', 'BE');
-
 // To avoid "Return value of TYPO3\CMS\Core\Core\Environment::isComposerMode() must be of the type bool, null returned"
 // in CountryService class
 TYPO3\CMS\Core\Core\Environment::initialize(
