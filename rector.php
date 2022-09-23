7fb71a8422b751d23c4fd638b72772dd67e0266b
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-09-23 21:08:27 +0200

    [TASK] Update rector to version 0.14.3

--- rector.php
+++ rector.php
@@ -2,49 +2,38 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::EARLY_RETURN);
-    $containerConfigurator->import(SetList::PHP_52);
-    $containerConfigurator->import(SetList::PHP_53);
-    $containerConfigurator->import(SetList::PHP_54);
-    $containerConfigurator->import(SetList::PHP_55);
-    $containerConfigurator->import(SetList::PHP_56);
-    $containerConfigurator->import(SetList::PHP_70);
-    $containerConfigurator->import(SetList::PHP_71);
-    $containerConfigurator->import(SetList::PHP_72);
-    $containerConfigurator->import(SetList::TYPE_DECLARATION);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_EXCEPTION);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_MOCK);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER);
+return static function (RectorConfig $config): void {
+    $config->import(LevelSetList::UP_TO_PHP_72);
+    $config->import(SetList::CODE_QUALITY);
+    $config->import(SetList::DEAD_CODE);
+    $config->import(SetList::EARLY_RETURN);
+    $config->import(SetList::TYPE_DECLARATION);
+    $config->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
+    $config->import(PHPUnitSetList::PHPUNIT_EXCEPTION);
+    $config->import(PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD);
+    $config->import(PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER);
 
-    $parameters = $containerConfigurator->parameters();
+    $config->phpVersion(PhpVersion::PHP_72);
 
-    $parameters->set(Option::PATHS, [
+    $config->autoloadPaths([
+        __DIR__ . '/.Build/vendor/autoload.php',
+    ]);
+    $config->bootstrapFiles([
+        __DIR__ . '/phpstan-bootstrap.php',
+    ]);
+    $config->paths([
         __DIR__ . '/Classes',
         __DIR__ . '/Configuration',
         __DIR__ . '/Tests',
     ]);
-
-    $parameters->set(Option::AUTOLOAD_PATHS, [__DIR__ . '/.Build/vendor/autoload.php']);
-
-    $parameters->set(Option::BOOTSTRAP_FILES, [
-        __DIR__ . '/phpstan-bootstrap.php',
-    ]);
-
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_72);
-
-    $parameters->set(Option::SKIP, [
+    $config->skip([
         RemoveUnusedPromotedPropertyRector::class, // Skip until compatibility with PHP >= 8.0
     ]);
 };
