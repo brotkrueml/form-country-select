e12c67016feb3ed917952ccf932e60691f6a1eb8
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-09-23 22:17:00 +0200

    [TASK] Reference translation file in form configuration
    See: https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/10.0/Breaking-87009-UseMultipleTranslationFilesByDefaultInEXTform.html

--- ext_tables.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('TYPO3') || die();
-
-use Brotkrueml\FormCountrySelect\Extension;
-
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:form/Resources/Private/Language/Database.xlf'][]
-    = 'EXT:' . Extension::KEY . '/Resources/Private/Language/Backend.xlf';
